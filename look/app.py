26e17d53052119b52ddadb8a468d668bd06501ea
Vishal Balasubramanian <vishal.bala.nps@gmail.com>
2019-12-27 11:36:41 +0530

    Initial Version

--- look/app.py
+++ look/app.py
@@ -1,3 +1,7 @@
 import falcon
+from .get_img import Resource
 
 application = falcon.API()
+
+apod_img = Resource()
+application.add_route('/get_img', apod_img)
--- look/get_img.py
+++ look/get_img.py
@@ -1,7 +1,16 @@
 import json
 import falcon
+import requests
+
 class Resource(object):
-    def get_img(self, request, response):
-        url = "google.com"
-        desc  "Google"
-        rvalue = '{"img_url": "'+url+'", desc: "'+desc+'"'
+    def on_get(self, req, resp):
+        apod_url = "https://api.nasa.gov/planetary/apod?api_key=DlArxIrdbCsDiAB2mA6Jo4m0PBFrWut8VSnkAQDe"
+        apod_resp = requests.get(apod_url)
+        apod_resp = json.loads(apod_resp.text)
+
+        url = apod_resp['url']
+        desc = apod_resp['explanation']
+        rvalue = '{"img_url": "'+url+'", "desc": "'+desc+'"'+"}"
+        resp.status = falcon.HTTP_200
+        resp.body = rvalue
+        
